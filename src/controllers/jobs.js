7b7a70631773378f817797f368e58441f2879c1e
Rafael Fonseca <rafael.life@gmail.com>
2022-02-13 19:26:39 -0300

    Add job payment with validations

--- src/controllers/jobs.js
+++ src/controllers/jobs.js
@@ -1,7 +1,7 @@
 const {Op} = require("sequelize");
 
 class JobsController {
-    async findUnpaid(req, res) {
+    async findUnpaidJobs(req, res) {
         const {Job, Contract} = req.app.get('models')
 
         const jobs = await Job.findAndCountAll({
@@ -33,8 +33,55 @@ class JobsController {
         return res.json(jobs)
     }
 
-    async findById(req, res) {
-        return false
+    async payJob(req, res) {
+        const profile = req.profile;
+
+        if (profile.type !== 'client') {
+            return res.status(400).json({
+                message: `You don't have the rights to execute this operation.`
+            })
+        }
+
+        const {Job, Contract, Profile} = req.app.get('models');
+
+        const job = await Job.findByPk(req.params.job_id, {
+            include: [
+                {
+                    model: Contract,
+                    where: {
+                        ClientId: profile.id,
+                        status: {
+                            [Op.or]: ['new', 'in_progress']
+                        }
+                    },
+                    required: true
+                }
+            ]
+        })
+
+        if (!job) {
+            return res.status(404).end()
+        }
+
+        const contractor = await Profile.findOne({where: {id: job.Contract.ContractorId}});
+
+        if (profile.balance < job.price) {
+            return res.status(400).json({
+                message: `You don't have enough credits to execute this operation.`
+            })
+        }
+
+        profile.balance -= job.price;
+        contractor.balance += job.price;
+        job.paid = true;
+
+        await profile.save();
+        await job.save();
+        await contractor.save();
+
+        return res.status(200).json({
+            message: 'Job paid successfully'
+        })
     };
 }
 
--- src/routes/jobs.js
+++ src/routes/jobs.js
@@ -3,8 +3,8 @@ const {getProfile} = require("../middleware/getProfile");
 const router = Router()
 const JobsController = require('../controllers/jobs');
 
-router.get('/jobs/unpaid', getProfile, JobsController.findUnpaid)
-router.post('/jobs/:job_id/pay', getProfile, JobsController.findById)
+router.get('/jobs/unpaid', getProfile, JobsController.findUnpaidJobs)
+router.post('/jobs/:job_id/pay', getProfile, JobsController.payJob)
 
 
 module.exports = router;
