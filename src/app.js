b24f64a56e8a5082c99ddb06dff735871f02b40a
Rafael Fonseca <rafael.life@gmail.com>
2022-02-13 18:11:14 -0300

    Add debug mode Fix find contract by id

--- src/app.js
+++ src/app.js
@@ -11,11 +11,25 @@ app.set('models', sequelize.models)
  * FIX ME!
  * @returns contract by id
  */
-app.get('/contracts/:id',getProfile ,async (req, res) =>{
-    const {Contract} = req.app.get('models')
+app.get('/contracts/:id', getProfile, async (req, res) => {
+    const {Contract, Profile} = req.app.get('models')
     const {id} = req.params
-    const contract = await Contract.findOne({where: {id}})
-    if(!contract) return res.status(404).end()
+    const contract = await Contract.findOne({
+        where: {id},
+        include: [
+            {
+                model: Profile,
+                as: 'Client',
+                where: {
+                    id: req.profile.id
+                },
+                required: true,
+                attributes: [] // Remove profile details
+            },
+        ],
+    })
+    if (!contract) return res.status(404).end()
     res.json(contract)
 })
+
 module.exports = app;
